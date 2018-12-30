aafffbc10a3f4a97ae8978217db7143b5ab7a27d
John Sparger <jsparger87@gmail.com>
2018-12-31 00:42:50 +0100

    Have a basic demo demonstrating all the tonejs controls I need for the first widget. Next it needs to be packaged into an actual widget.

--- src/webics.js
+++ src/webics.js
@@ -3,13 +3,56 @@ import Tone from 'tone';
 // remembering how exports work...
 var man = 10;
 
-// something is not quite right with the import of tone. probably some option
-// in the commonjs rollup plugin. In any case, play an 8th note with pitch C4.
-// var synth = new Tone.__moduleExports.Synth().toMaster();
-var synth = new Tone.Synth().toMaster();
-synth.triggerAttackRelease("C4", "8n");
+// A quick demo
+//set the bpm and time signature first
+Tone.Transport.timeSignature = [6, 4];
+Tone.Transport.bpm.value = 30;
+
+//a little reverb
+var reverb = new Tone.Freeverb({
+  "roomSize" : 0.2,
+  "wet" : 0.3
+}).toMaster();
+
+// a pitch shift control
+var pitchShift = new Tone.PitchShift().connect(reverb);
+
+//the synth settings
+var synthSettings = {
+  "oscillator": {
+    "detune": 0,
+    "type": "custom",
+    "partials" : [2, 1, 2, 2],
+    "phase": 0,
+    "volume": 0
+  },
+  "envelope": {
+    "attack": 0.005,
+    "decay": 0.3,
+    "sustain": 0.2,
+    "release": 1,
+  },
+  "portamento": 0.01,
+  "volume": -20
+};
+
+// create the synth.
+var synth = new Tone.Synth(synthSettings).connect(pitchShift);
+
+// create the sequence
+var seq = new Tone.Sequence(function(time, note){
+			synth.triggerAttackRelease(note, "8n", time);
+		}, ["E4", "F#4", "B4", "C#5", "D5", "F#4", "E4", "C#5", "B4", "F#4", "D5", "C#5"], "8n").start();
+
+// start the "song"
+Tone.Transport.start("+0.1");
+
+// ramp the tempo up to 180 over 10 seconds.
+Tone.Transport.bpm.linearRampTo(180, 10);
 
 export {
   man,
+  seq,
+  pitchShift,
   Tone,
 };
