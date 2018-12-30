d4c94bdbaae8071eeef243b4ced0d7636181e11c
John Sparger <jsparger87@gmail.com>
2018-12-30 22:48:02 +0100

    was loading the Tone module incorrectly. Things looking better now.

--- src/webics.js
+++ src/webics.js
@@ -1,11 +1,12 @@
-import * as Tone from 'tone';
+import Tone from 'tone';
 
 // remembering how exports work...
 var man = 10;
 
 // something is not quite right with the import of tone. probably some option
 // in the commonjs rollup plugin. In any case, play an 8th note with pitch C4.
-var synth = new Tone.__moduleExports.Synth().toMaster();
+// var synth = new Tone.__moduleExports.Synth().toMaster();
+var synth = new Tone.Synth().toMaster();
 synth.triggerAttackRelease("C4", "8n");
 
 export {
