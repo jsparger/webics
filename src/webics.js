6452c668408338fd7128230a60b1bcf3e60c1e37
John Sparger <jsparger87@gmail.com>
2019-01-01 01:51:33 +0100

    Have a basic demo demonstrating all the tonejs controls I need for the first widget. Next it needs to be packaged into an actual widget.

--- src/webics.js
+++ src/webics.js
@@ -10,39 +10,39 @@ Tone.Transport.bpm.value = 30;
 
 //a little reverb
 var reverb = new Tone.Freeverb({
-  "roomSize" : 0.2,
-  "wet" : 0.3
+  "roomSize" : 0.45,
+  "wet" : 1.0
 }).toMaster();
 
 // a pitch shift control
-var pitchShift = new Tone.PitchShift().connect(reverb);
+// var pitchShift = new Tone.PitchShift().connect(reverb);
 
 //the synth settings
 var synthSettings = {
   "oscillator": {
-    "detune": 0,
-    "type": "custom",
-    "partials" : [2, 1, 2, 2],
-    "phase": 0,
-    "volume": 0
+    // "detune": 0,
+    "type": "triangle",
+    // "partials" : [2, 1, 2, 2],
+    // "phase": 0,
+    // "volume": 0
   },
-  "envelope": {
-    "attack": 0.005,
-    "decay": 0.3,
-    "sustain": 0.2,
-    "release": 1,
-  },
-  "portamento": 0.01,
-  "volume": -20
+  // "envelope": {
+  //   "attack": 0.005,
+  //   "decay": 0.3,
+  //   "sustain": 0.2,
+  //   "release": 1,
+  // },
+  "portamento": 0.05,
+  // "volume": -20
 };
 
 // create the synth.
-var synth = new Tone.Synth(synthSettings).connect(pitchShift);
+var synth = new Tone.Synth(synthSettings).connect(reverb);
 
 // create the sequence
 var seq = new Tone.Sequence(function(time, note){
 			synth.triggerAttackRelease(note, "8n", time);
-		}, ["E4", "F#4", "B4", "C#5", "D5", "F#4", "E4", "C#5", "B4", "F#4", "D5", "C#5"], "8n").start();
+		}, ["E1", "F#2", "B2", "C#3", "D3", "F#4", "E1", "C#3", "B2", "F#4", "D4", "G#3"], "8n").start();
 
 // start the "song"
 Tone.Transport.start("+0.1");
@@ -51,8 +51,10 @@ Tone.Transport.start("+0.1");
 Tone.Transport.bpm.linearRampTo(180, 10);
 
 export {
+  synth,
+  reverb,
   man,
   seq,
-  pitchShift,
+  // pitchShift,
   Tone,
 };
