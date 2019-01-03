6b0e825108b22181431a355b86731cec4535a910
John Sparger <jsparger87@gmail.com>
2019-01-03 15:52:12 +0100

    added a Poller and tested connection to a webservice serving PVs. Need probably another level of abstraction.

--- src/webics.js
+++ src/webics.js
@@ -1,60 +1,172 @@
 import Tone from 'tone';
 
-// remembering how exports work...
-var man = 10;
-
 // A quick demo
 //set the bpm and time signature first
 Tone.Transport.timeSignature = [6, 4];
 Tone.Transport.bpm.value = 30;
 
-//a little reverb
-var reverb = new Tone.Freeverb({
-  "roomSize" : 0.2,
-  "wet" : 0.9
-}).toMaster();
+// //a little reverb
+// var reverb = new Tone.Freeverb({
+//   "roomSize" : 0.2,
+//   "wet" : 0.9
+// }).toMaster();
+//
+// // a pitch shift control
+// var pitchShift = new Tone.PitchShift({
+//   "wet" : 1.0,
+// }).connect(reverb);
+//
+// //the synth settings
+// var synthSettings = {
+//   "oscillator": {
+//     // "detune": 0,
+//     "type": "sine",
+//     // "partials" : [2, 1, 2, 2],
+//     // "phase": 0,
+//     // "volume": 0
+//   },
+//   // "envelope": {
+//   //   "attack": 0.005,
+//   //   "decay": 0.3,
+//   //   "sustain": 0.2,
+//   //   "release": 1,
+//   // },
+//   "portamento": 0.05,
+//   // "volume": -20
+// };
+//
+// // create the synth.
+// var synth = new Tone.Synth(synthSettings).connect(pitchShift);
+//
+// // create the sequence
+// var seq = new Tone.Sequence(function(time, note){
+// 			synth.triggerAttackRelease(note, "8n", time);
+// 		}, ["E1", "F2", "F#3", "G4", "D#3", "F4", "G#4", "A#4", "A3", "F#3", "D3", "A#2"], "8n").start();
+//
+// // start the "song"
+// Tone.Transport.start("+0.1");
+//
+// // ramp the tempo up to 180 over 10 seconds.
+// Tone.Transport.bpm.linearRampTo(180, 10);
 
-// a pitch shift control
-// var pitchShift = new Tone.PitchShift().connect(reverb);
+function convert(value, zero, slope) {
+  return (value - zero)/(slope);
+}
 
-//the synth settings
-var synthSettings = {
-  "oscillator": {
-    // "detune": 0,
-    "type": "sine",
-    // "partials" : [2, 1, 2, 2],
-    // "phase": 0,
-    // "volume": 0
-  },
-  // "envelope": {
-  //   "attack": 0.005,
-  //   "decay": 0.3,
-  //   "sustain": 0.2,
-  //   "release": 1,
-  // },
-  "portamento": 0.05,
-  // "volume": -20
+function clamp(value, min=0, max=1) {
+  return Math.min(Math.max(value,min),max);
+}
+
+// convert from one unit to another
+class UnitConversion {
+  constructor(zero=0, slope=1) {
+    this._zero = zero;
+    this._slope = slope;
+  }
+  apply(value) {
+    return convert(value, this._zero, this._slope);
+  }
 };
 
-// create the synth.
-var synth = new Tone.Synth(synthSettings).connect(reverb);
+// A class which automatically applies a unit conversion and calls a callback
+class Adapter {
+  constructor(conversion, callback) {
+    this._conversion = conversion;
+    this._callback = callback;
+    this._input = null;
+    this._output = null;
+  }
+  get output() { return this._output; }
+  get input() { return this._input; }
+  set input(value) {
+    this._input = value;
+    this._output = this._conversion.apply(this._input);
+    this._callback(this._input);
+  }
+};
 
-// create the sequence
-var seq = new Tone.Sequence(function(time, note){
-			synth.triggerAttackRelease(note, "8n", time);
-		}, ["E1", "F2", "F#3", "G4", "D#3", "F4", "G#4", "A#4", "A3", "F#3", "D3", "A#2"], "8n").start();
+class SoundWidget {
+  constructor(options) {
+    this.tempo = new Adapter(options['tempo']['conversion'], this._set_tempo.bind(this));
+    this.effect = new Adapter(options['pitch']['conversion'], this._set_effect.bind(this));
+    this.pedal = new Tone.Chorus({spread: 60}).toMaster();
+    // this.compressor = new Tone.Compressor().toMaster():
+    // this.effect = new Tone.Filter({rolloff: -48, type: "bandpass"}).connect(this.compressor);
+    // this.synth = new Tone.Synth().toMaster();
+    // this.seq = new Tone.Sequence((time, note) => {
+    // 			this.synth.triggerAttackRelease(note, "8n", time);
+    // 		}, ["C3",], "8n").start();
+    this.player = new Tone.Player({
+      url : "/files/boom_bap_drum.wav",
+      loop : true,
+    }).connect(this.pedal);
 
-// start the "song"
-Tone.Transport.start("+0.1");
+    Tone.Buffer.on('load',this.enable.bind(this));
+  }
 
-// ramp the tempo up to 180 over 10 seconds.
-Tone.Transport.bpm.linearRampTo(180, 10);
+  enable(value=true) {
+    value ? this.player.start() : this.player.stop();
+  }
+
+  _set_tempo(value) {
+    // Tone.Transport.bpm.linearRampTo(value, 1);
+    this.player.playbackRate = value;
+  }
+
+  _set_effect(value) {
+    this.pedal.wet.linearRampTo(value, 1);
+  }
+};
+
+class Poller {
+  constructor(url, interval, callback, enabled=true) {
+    this.url = url;
+    this.interval = interval;
+    this.callback = callback;
+    this.enable(enabled);
+  }
+
+  async poll() {
+    if (!this.enabled) return;
+    let json = await (await fetch(this.url)).json();
+    this.callback(json);
+    setTimeout(this.poll.bind(this), this.interval);
+  }
+
+  enable(value=true) {
+    this.enabled = value;
+    this.poll();
+  }
+};
+
+
+let widget = new SoundWidget({
+    tempo: {
+        conversion: new UnitConversion(),
+    },
+    pitch: {
+        conversion: new UnitConversion(),
+    }
+});
+
+Tone.Transport.start();
+
+let speed_poller = new Poller("http://localhost:5000/pv/LabS-VIP:Chop-Drv-01:Spd", 1000, (json) => {
+	console.log(json.value);
+  let fraction = json.value/7.0;
+	webics.widget.tempo.input = fraction > 0.01 ? fraction + 0.3 : 0;
+});
 
 export {
-  synth,
-  reverb,
-  man,
-  seq,
+  // synth,
+  // reverb,
+  // man,
+  // seq,
   // pitchShift,
   Tone,
+  SoundWidget,
+  UnitConversion,
+  widget,
+  Poller,
+  speed_poller,
 };
